61a986f2b8fc92b014c4dfc87e2adc5576323e4d
sjPark00 <sjpark513@gm.gist.ac.kr>
2022-11-24 16:00:49 +0900

    create varialbes.ts

---
+++ client/src/util/parser/variables.ts
@@ -0,0 +1,14 @@
+export const SEPARATOR: string[] = ["{", "}", "[", "]", ",", ":"];
+
+export const DEFINEKEYWORD: any = {
+    "[": "array",
+    "{": "object",
+    ",": "seperator",
+    ":": "colon",
+    '"': "string",
+    n: "null_object",
+    f: "boolean",
+    t: "boolean",
+    "]": "close_array",
+    "}": "close_object",
+};
