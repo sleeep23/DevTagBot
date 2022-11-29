d608fb03f590cbc5882d3f8a2e3c055100752389
sjPark00 <sjpark513@gm.gist.ac.kr>
2022-11-29 20:03:46 +0900

    create lexicalizer and errors

--- client/src/util/JSON-parser/ErrorHandling/Error.ts
+++ client/src/util/JSON-parser/ErrorHandling/Error.ts
@@ -5,3 +5,17 @@ export const syntaxErrorMessage = (index: number) => {
 export const multipleDecimalPointsErrorMessage = (points: number) => {
   return new SyntaxError("👯‍ Multiple decimal points! : " + points);
 };
+
+
+// errors for Lexcialize.ts
+export const arrayDepthErrorMessage = (index: number) => {
+  return new SyntaxError("array depth error: " + index);
+};
+
+export const objectDepthErrorMessage = (index: number) => {
+  return new SyntaxError("object depth error: " + index);
+};
+
+export const quoteErrorMessage = () => {
+  return new SyntaxError('there are odd numbers of"');
+}
